86abc2a8081ce6fb66f9124f11b36f5603357cd5
Christian Wollny <dev.chwo@gmail.com>
2015-04-03 16:37:03 +0200

    HashtagTimelineVC: get Twitter Account Access

--- TwitterTimeline/TwitterTimeline/HashtagTimelineVC.swift
+++ TwitterTimeline/TwitterTimeline/HashtagTimelineVC.swift
@@ -7,29 +7,30 @@
 //
 
 import UIKit
+import Accounts
 
 class HashtagTimelineVC: UIViewController {
+    
+    // Twitter Account
+    let accountStore = ACAccountStore()
+    let twitterType = ACAccountStore().accountTypeWithAccountTypeIdentifier(ACAccountTypeIdentifierTwitter)
+    var twitterAccount: ACAccount?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // get Twitter Account Access
+        accountStore.requestAccessToAccountsWithType(twitterType, options: nil) {
+            success, error in
+            if success {
+                let alleAccounts = self.accountStore.accountsWithAccountType(self.twitterType)
+                if alleAccounts.count > 0 {
+                    self.twitterAccount = alleAccounts.last as ACAccount?
+                }
+            }else{
+                println(error.localizedDescription)
+            }
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
